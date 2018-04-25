fb6b9d4217ed8985bfbdb2359ab6eff4232c3604
Yicong M. Gong <gongyc3@gmail.com>
2018-04-25 14:10:59 -0700

    rename

---
+++ WebApps/helloApp/application.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from flask import jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, current_app
+import json
+import requests
+AU_ENDPOINT = 'http://0.0.0.0:2000'
+AT_ENDPOINT = 'http://0.0.0.0:3000'
+RE_ENDPOINT = 'http://0.0.0.0:4000'
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def test():
+    if request.method == 'GET':
+        
+        url = AU_ENDPOINT # define end-point
+
+        params = {'test' : 'test'}
+        data = requests.get(url, params = params)
+        data = data.json()
+        
+        if data['status'] == 'approved':
+            url = AT_ENDPOINT # define end-point
+            params = {'test' : 'test'}
+            data = requests.get(url, params = params)
+            data = data.json()
+            return data['test']
+        else:
+            return '', 404
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000)
